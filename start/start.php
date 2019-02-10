0c50691b23d2ed8172fbc6a8357cc9a5d92128e2
dongasai <1514582970@qq.com>
2019-02-10 10:02:45 +0800

    升级

--- start/start.php
+++ start/start.php
@@ -27,9 +27,9 @@ $loader->register();
 
 $server = new \pms\Server('0.0.0.0', 9502, SWOOLE_BASE, SWOOLE_SOCK_TCP, [
     'daemonize' => false,
-    'reactor_num_mulriple' => 2,
-    'worker_num_mulriple' => 4,
-    'task_worker_num_mulriple' => 20,
+    'reactor_num_mulriple' => 1,
+    'worker_num_mulriple' => 2,
+    'task_worker_num_mulriple' => 5,
     'reload_async' => false,
     'open_eof_split' => true, //打开EOF检测
     'package_eof' => PACKAGE_EOF, //设置EOF
