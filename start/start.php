abf491a1bf5143b40d010fe4ce65e1b75d5d91e0
dongasai <1514582970@qq.com>
2019-02-18 20:50:09 +0800

    修正更新

--- start/start.php
+++ start/start.php
@@ -6,8 +6,8 @@ define('ROOT_DIR', dirname(__DIR__));
 
 require ROOT_DIR . '/vendor/autoload.php';
 # 进行一些项目配置
-define('APP_SECRET_KEY', get_env("APP_SECRET_KEY"));
-$re9 = env_exist([
+define('APP_SECRET_KEY', \pms\get_env("APP_SECRET_KEY"));
+$re9 = \pms\env_exist([
     'GCACHE_HOST', 'GCACHE_PORT', 'GCACHE_AUTH', 'GCACHE_PERSISTENT', 'GCACHE_PREFIX', 'GCACHE_INDEX',
     'MYSQL_HOST', 'MYSQL_PORT', 'MYSQL_DBNAME', 'MYSQL_PASSWORD', 'MYSQL_USERNAME']);
 if (is_string($re9)) {
@@ -31,8 +31,6 @@ $server = new \pms\Server('0.0.0.0', 9502, SWOOLE_BASE, SWOOLE_SOCK_TCP, [
     'worker_num_mulriple' => 2,
     'task_worker_num_mulriple' => 5,
     'reload_async' => false,
-    'open_eof_split' => true, //打开EOF检测
-    'package_eof' => PACKAGE_EOF, //设置EOF
 ]);
 
 $guidance = new \app\Guidance();
